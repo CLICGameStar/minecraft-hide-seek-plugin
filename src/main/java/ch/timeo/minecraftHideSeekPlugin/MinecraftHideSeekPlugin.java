c8431b8b4130a16103309bca91bda316e30dd0bc
Tim <timeo.golaz@epfl.ch>
2025-11-21 12:49:25 +0100

    chore: initial project structure

---
+++ src/main/java/ch/timeo/minecraftHideSeekPlugin/MinecraftHideSeekPlugin.java
@@ -0,0 +1,17 @@
+package ch.timeo.minecraftHideSeekPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftHideSeekPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
